028a79b58f40960b188cc4bad9bff967dc7db570
gojnimer <gojnimer@gmail.com>
2024-10-25 21:49:23 +0000

    feat: update bucket file fetching logic and adjust image source

--- src/services/buckets/fetch_bucket_files.ts
+++ src/services/buckets/fetch_bucket_files.ts
@@ -1,9 +1,23 @@
+import minioClient from "@/lib/minio";
+import { MINIO_API_URL } from "astro:env/server";
+
+const generatePublicMinioUrl = (bucket: string, object: string) => {
+  return `https://${MINIO_API_URL}/${bucket}/${object}`;
+}
 
 export default async function fetchBucketFiles(
   bucketName: string,
   folderName = ""
 ) {
-  var images: string[] = [];
 
-  return images;
+  const images: string[] = await new Promise((resolve, reject) => {
+    const objectsListTemp: any[] = [];
+    const stream = minioClient.listObjectsV2(bucketName, folderName, true, '');
+    stream.on('data', obj => objectsListTemp.push(generatePublicMinioUrl(bucketName, obj.name as string)));
+    stream.on('error', reject);
+    stream.on('end', () => {
+      resolve(objectsListTemp);
+    });
+  });
+  return images.slice(0, 10);
 }
