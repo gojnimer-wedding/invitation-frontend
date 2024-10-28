43972afb1151b55d931c24993de859a9fcf1143b
gojnimer <gojnimer@gmail.com>
2024-10-28 17:22:29 +0000

    fix: adjust exit animation properties in FlipWords component

--- src/components/ui/flip-words.tsx
+++ src/components/ui/flip-words.tsx
@@ -51,10 +51,8 @@ export const FlipWords = ({
         }}
         exit={{
           opacity: 0,
-          y: -40,
           x: 40,
           filter: "blur(8px)",
-          scale: 2,
           position: "absolute",
         }}
         className={cn(
