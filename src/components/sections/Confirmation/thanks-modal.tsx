069dcddec72717cbdf66615e16491f80fd372095
gojnimer <gojnimer@gmail.com>
2024-11-30 00:03:06 +0000

    refactor: remove Confirmation component and update related imports and styles

--- src/components/sections/Confirmation/thanks-modal.tsx
+++ src/components/sections/Confirmation/thanks-modal.tsx
@@ -26,7 +26,7 @@ const ThanksModal = forwardRef(function ThanksModal(_, ref) {
                 inesquecível, e sua companhia o tornará ainda mais especial. 💕
               </p>
             </div>
-            <div className="mt-6 space-y-2 px-4">
+            <div className="mt-6 space-y-2 px-4 mb-6">
               <p className="mt-4 text-gray-600 text-start text-[1rem]">
                 Para te ajudar com os preparativos, aqui estão alguns links
                 úteis:
@@ -44,8 +44,8 @@ const ThanksModal = forwardRef(function ThanksModal(_, ref) {
                     🎁 Lista de Presentes
                   </span>
                 </a>
-                {/*                 <a
-                  href="https://noivos.casar.com/karine-e-guilherme-2025-01-17"
+                <a
+                  href="https://revista.icasei.com.br/traje-esporte-fino/"
                   className="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-[#db7b8d] rounded-xl group"
                 >
                   <span className="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-[#da6379] rounded group-hover:-mr-4 group-hover:-mt-4">
@@ -53,17 +53,11 @@ const ThanksModal = forwardRef(function ThanksModal(_, ref) {
                   </span>
                   <span className="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-[#db7b8d] rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
                   <span className="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">
-                    🎁 Mapa do Local
+                    🤵 Guia de trajes
                   </span>
-                </a> */}
+                </a>
               </div>
             </div>
-
-            {/*          <div className="mt-6 text-center mb-10">
-              <p className="text-gray-600">
-                Dúvidas? <span>Entre em contato conosco!</span> 💌
-              </p>
-            </div> */}
           </div>
         </Drawer.Content>
       </Drawer.Portal>
